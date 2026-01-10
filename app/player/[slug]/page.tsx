45250907b7b9a2f85d38c44486b214298a4ffab4
Jacopo Smeraldi <106149468+EmeraldsHub@users.noreply.github.com>
2026-01-10 16:40:18 +0000

    small fix

--- app/player/[slug]/page.tsx
+++ app/player/[slug]/page.tsx
@@ -1,6 +1,7 @@
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 export const fetchCache = "force-no-store";
+export const dynamicParams = true;
 
 import { PlayerSyncButton } from "@/components/PlayerSyncButton";
 import { RankIcon } from "@/components/RankIcon";
